9a2fcf2972956f34600b88cac522de15f6867932
kiyaboapp <kiyaboapp@gmail.com>
2025-11-21 02:43:08 +0300

    I Love you, my Dafrosa

--- kiyabo32.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-c:\KiyaboAppPython\python32\python.exe c:\KiyaboAppPython\run_cli.py %*
--- kiyabo64.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-c:\KiyaboAppPython\python64\python.exe c:\KiyaboAppPython\run_cli.py %*
